7c066598a182215e137f60b54e6304f097e294a1
rick tait <rickt@rickt.org>
2016-03-03 23:58:50 -0800

    first commit of timebot-simple

---
+++ README.md
@@ -0,0 +1,10 @@
+simple go app that returns a nice string with the current time in Los Angeles, Japan & London/UTC.
+
+if request = / redirect to /time
+
+/time takes a parameter, e.g. ?place=lax or ?place=jpn or ?place=utc
+
+this app is currently running & deployed at http://timebot-simple.appspot.com/
+
+-RMT
+
---
+++ timebot-simple.go
@@ -0,0 +1,66 @@
+// http://timebot-simple.appspot.com/time
+// http://timebot-simple.appspot.com/time?place=lax (etc)
+
+package timebot-simple
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// constants
+const (
+	shortformat = "3:04pm"
+)
+
+// types
+type times struct {
+	jpn time.Time
+	lax time.Time
+	utc time.Time
+}
+
+func init() {
+	// setup the http handlers
+	http.HandleFunc("/", handler_redirect)
+	http.HandleFunc("/time", handler_time)
+}
+
+// get the current time in JPN, LAX, UTC and return them in a struct of 3x t.Time
+func getTime() times {
+	var tzu times
+	t := time.Now()
+	// locations
+	loc_lax, _ := time.LoadLocation("America/Los_Angeles")
+	loc_jpn, _ := time.LoadLocation("Japan")
+	loc_utc, _ := time.LoadLocation("UTC")
+	// times
+	tzu.lax = t.In(loc_lax)
+	tzu.jpn = t.In(loc_jpn)
+	tzu.utc = t.In(loc_utc)
+	return tzu
+}
+
+// redirect
+func handler_redirect(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "http://timebot-simple.appspot.com/time", 302)
+}
+
+// print the current time in Japan, LA, London/UTC
+func handler_time(w http.ResponseWriter, r *http.Request) {
+	var mytimes times
+	mytimes = getTime()
+	// was there a parameter?
+	place := r.URL.Query().Get("place")
+	switch place {
+	case "lax":
+		fmt.Fprintf(w, mytimes.lax.Format(shortformat)+" in Los Angeles ("+mytimes.jpn.Format(shortformat)+" in Japan,  "+mytimes.utc.Format(shortformat)+" in London/UTC)\n")
+	case "jpn":
+		fmt.Fprintf(w, mytimes.jpn.Format(shortformat)+" in Japan ("+mytimes.lax.Format(shortformat)+" in Los Angeles, "+mytimes.utc.Format(shortformat)+" in London/UTC)\n")
+	case "utc":
+		fmt.Fprintf(w, mytimes.utc.Format(shortformat)+" in London/UTC ("+mytimes.lax.Format(shortformat)+" in Los Angeles, "+mytimes.jpn.Format(shortformat)+" in Japan)\n")
+	default:
+		fmt.Fprintf(w, mytimes.lax.Format(shortformat)+" in Los Angeles ("+mytimes.jpn.Format(shortformat)+" in Japan, "+mytimes.utc.Format(shortformat)+" in London/UTC)\n")
+	}
+}
