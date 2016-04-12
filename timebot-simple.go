7591c3135c43e864e4dca98be3c4b02a17b97cc2
rick tait <rickt@rickt.org>
2016-04-11 18:30:36 -0700

    cleanup

--- timebot-simple.go
+++ timebot-simple.go
@@ -1,5 +1,6 @@
-// http://timebot-simple.appspot.com/time
-// http://timebot-simple.appspot.com/time?place=lax (etc)
+// http://timebot-simple.appspot.com/time?tz=America/Los_Angeles
+// http://timebot-simple.appspot.com/time?tz=Asia/Tokyo
+// http://timebot-simple.appspot.com/time?tz=Asia/UTC (etc)
 
 package timebotsimple
 
@@ -14,36 +15,38 @@ const (
 	shortformat = "3:04pm"
 )
 
-// types
-type times struct {
-	jpn time.Time
-	lax time.Time
-	utc time.Time
-}
-
+// there's no main() func in google app engine
 func init() {
 	// setup the http handlers
 	http.HandleFunc("/", handler_redirect)
 	http.HandleFunc("/time", handler_time)
 }
 
-// redirect
+// redirect / to /time
 func handler_redirect(w http.ResponseWriter, r *http.Request) {
-	// redirect / to /time
 	http.Redirect(w, r, "/time", 302)
 }
 
 // print the current time in Japan, LA, London/UTC
 func handler_time(w http.ResponseWriter, r *http.Request) {
 	// example call from slack
-	// /time?place=jpn&token=REDACTED&team_id=REDACTED&team_domain=REDACTED&channel_id=REDACTED&channel_name=directmessage&user_id=REDACTED&user_name=REDACTED&command=%2Fjapantime&text=&response_url=REDACTED
-	place := r.URL.Query().Get("place")
-	// user := r.URL.Query().Get("user_name")
-	switch place {
+	// /time?tz=Asia/Tokyo&token=REDACTED&team_id=REDACTED&team_domain=REDACTED&channel_id=REDACTED&channel_name=directmessage&user_id=REDACTED&user_name=REDACTED&command=%2Fjapantime&text=&response_url=REDACTED
+	tz := r.URL.Query().Get("tz")
+	switch tz {
 	case "healthcheck":
 		fmt.Fprintf(w, "ok\n")
+		return
+	case "":
+		loc, _ := time.LoadLocation("America/Los_Angeles")
+		fmt.Fprintf(w, time.Now().In(loc).Format(shortformat)+" in "+tz+"\n")
+		return
 	default:
-		loc, _ := time.LoadLocation(place)
-		fmt.Fprintf(w, time.Now().In(loc).Format(shortformat)+" in "+place+"\n")
+		loc, err := time.LoadLocation(tz)
+		if err != nil {
+			fmt.Fprintf(w, "I'm sorry, but I was not able to find a timezone matching \""+tz+"\"! :confused: \n")
+			return
+		}
+		fmt.Fprintf(w, time.Now().In(loc).Format(shortformat)+" in "+tz+"\n")
+		return
 	}
 }
